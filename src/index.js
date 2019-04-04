ca417aa144b00f5d0bd794f9c39790d3c2c41c1d
Karol Masiak <karol.masiak@olmero.ch>
2019-04-04 18:37:35 +0200

    Added the dom-repeat binding

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import '@polymer/iron-icons';
 
 import '@polymer/polymer';
 import '@polymer/polymer/lib/elements/dom-if';
+import '@polymer/polymer/lib/elements/dom-repeat';
 import '@polymer/polymer/lib/elements/dom-bind';
 import '@polymer/polymer/lib/elements/dom-module';
 import '@granite-elements/granite-bootstrap/granite-bootstrap';
