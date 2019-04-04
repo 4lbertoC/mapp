3c993b2d79bd1eba9aec4fa682aec1edacfa0f0e
Alberto Congiu <4lbertoc@gmail.com>
2019-04-04 18:35:14 +0200

    added search field to schedule page

--- src/components/containers/mapp-schedule/index.js
+++ src/components/containers/mapp-schedule/index.js
@@ -1,9 +1,9 @@
 
 import {PolymerElement, html} from '@polymer/polymer/polymer-element';
 
-// import css from './style.pcss';
-// import template from './template.html';
-// import '../../dumbs/sk-button';
+import css from './style.pcss';
+import boostrapOverrides from '../../../styles/bootstrap-overrides.pcss';
+import template from './template.html';
 
 export default class MappSchedule extends PolymerElement {
   static get properties() {
@@ -11,7 +11,7 @@ export default class MappSchedule extends PolymerElement {
   }
 
   static get template() {
-    return html([`<style></style> schedule`]);
+    return html([`<style include="granite-bootstrap"></style> <style>${boostrapOverrides}</style> <style>${css}</style> ${template}`]);
   }
 }
 
