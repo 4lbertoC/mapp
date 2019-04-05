157a23ba79bf5ae7f52abd9f0297cbc87e7b6a8a
Karol Masiak <karol.masiak@olmero.ch>
2019-04-05 13:53:29 +0200

    Added the sessions endpoint implementation

--- src/components/containers/mapp-dashboard/index.js
+++ src/components/containers/mapp-dashboard/index.js
@@ -8,23 +8,21 @@ export default class MappDashboard extends PolymerElement {
   connectedCallback() {
     super.connectedCallback();
 
-    fetch('https://mapp.hackathon2019.dev.tda.link/v1/sessions').then(function (response) {
+    fetch('https://mapp.hackathon2019.dev.tda.link/v1/sessions').then( (response) => {
       return response.json();
-    })
-      .then(function (sessions) {
-        console.log(JSON.stringify(myJson));
+    }).then( (sessions) => {
         let mentorSessions = [];
 
         for (const item of sessions) {
           let session = {};
           session = item.mentor;
           session.skills = [item.skill];
-          session.startDateTime = item.timeSlot;
+          session.startDateTime = item.timeSlot.date;
           mentorSessions.push(session);
         }
 
         this.set('mentors', mentorSessions);
-       });
+      });
   }
 
   static get properties() {
@@ -32,19 +30,19 @@ export default class MappDashboard extends PolymerElement {
       mentors: {
         value() {
           return [
-            {firstName: 'Karol', lastName: 'Masiak', skills: [{name: 'Java'}], startDateTime: '2019-01-05T10:00:00',},
-            {
-              firstName: 'Damir',
-              lastName: 'Cohadarevic',
-              skills: [{name: 'Bots'}],
-              startDateTime: '2019-01-05T13:00:00'
-            },
-            {
-              firstName: 'Michael',
-              lastName: 'Ehleben',
-              skills: [{name: 'Drawing'}],
-              startDateTime: '2019-01-05T15:00:00'
-            },
+            // {firstName: 'Karol', lastName: 'Masiak', skills: [{name: 'Java'}], startDateTime: '2019-01-05T10:00:00',},
+            // {
+            //   firstName: 'Damir',
+            //   lastName: 'Cohadarevic',
+            //   skills: [{name: 'Bots'}],
+            //   startDateTime: '2019-01-05T13:00:00'
+            // },
+            // {
+            //   firstName: 'Michael',
+            //   lastName: 'Ehleben',
+            //   skills: [{name: 'Drawing'}],
+            //   startDateTime: '2019-01-05T15:00:00'
+            // },
           ];
         }
       }
