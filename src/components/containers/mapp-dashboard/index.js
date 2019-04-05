6cb815632648b5a752e046b521119b0e00b8a010
Karol Masiak <karol.masiak@olmero.ch>
2019-04-05 13:30:16 +0200

    Implemented the sessions endpoint

--- src/components/containers/mapp-dashboard/index.js
+++ src/components/containers/mapp-dashboard/index.js
@@ -4,14 +4,47 @@ import css from './style.pcss';
 import template from './template.html';
 
 export default class MappDashboard extends PolymerElement {
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    fetch('https://mapp.hackathon2019.dev.tda.link/v1/sessions').then(function (response) {
+      return response.json();
+    })
+      .then(function (sessions) {
+        console.log(JSON.stringify(myJson));
+        let mentorSessions = [];
+
+        for (const item of sessions) {
+          let session = {};
+          session = item.mentor;
+          session.skills = [item.skill];
+          session.startDateTime = item.timeSlot;
+          mentorSessions.push(session);
+        }
+
+        this.set('mentors', mentorSessions);
+       });
+  }
+
   static get properties() {
     return {
       mentors: {
         value() {
           return [
-            { firstName: 'Karol', lastName: 'Masiak', skills: [{ name: 'Java' }], startDateTime: '2019-01-05T10:00:00', },
-            { firstName: 'Damir', lastName: 'Cohadarevic', skills: [{ name: 'Bots' }], startDateTime: '2019-01-05T13:00:00' },
-            { firstName: 'Michael', lastName: 'Ehleben', skills: [{ name: 'Drawing' }], startDateTime: '2019-01-05T15:00:00' },
+            {firstName: 'Karol', lastName: 'Masiak', skills: [{name: 'Java'}], startDateTime: '2019-01-05T10:00:00',},
+            {
+              firstName: 'Damir',
+              lastName: 'Cohadarevic',
+              skills: [{name: 'Bots'}],
+              startDateTime: '2019-01-05T13:00:00'
+            },
+            {
+              firstName: 'Michael',
+              lastName: 'Ehleben',
+              skills: [{name: 'Drawing'}],
+              startDateTime: '2019-01-05T15:00:00'
+            },
           ];
         }
       }
